3020481c8712c22f1621323b6e87b10c3ddf04c4
Zhenyong Chen <b07273@freescale.com>
2013-06-05 10:34:47 +0800

    ENGR00265630 Support different installation directory
    Daily build script requires the libraries to be installed in a different
    directory other than default /usr. Use prefix=<dir> to support this
    requirement.
    
    Date: 2013-06-05
    Signed-off-by: Zhenyong Chen <b07273@freescale.com>

--- fastbuild.sh
+++ fastbuild.sh
@@ -6,7 +6,8 @@
 # for clean, use "./fastbuild clean"
 # for make, use "./fastbuild"
 # for make non-dri version, use "./fastbuild NODRI=1"
-# for install, use "sudo ./fastbuild.sh install"
+# for install, use "sudo ./fastbuild.sh install". Default path: /usr. 
+#   To change to other place, use "prefix=<absolute dir>"
 
 make -C EXA/src/ -f makefile.linux $* || exit 1
 make -C DRI_1.10.4/src/ -f makefile.linux $* || exit 1
