c18909e8457b3dd2647b4021525a4ad93dc9c2df
Shawn Xiao <b49994@freescale.com>
2015-08-28 11:57:03 +0800

    MGS-959 [#ccc] License Changing in EXA source code
    Since 5.0.11.p7, Vivante change the EXA license from GPL to MIT. Need
    update this change in our code too.
    
    Date Aug 28, 2015
    
    Signed-off-by: Shawn Xiao <b49994@freescale.com>

--- EXA/src/vivante_fbdev/vivante_fbdev_driver.c
+++ EXA/src/vivante_fbdev/vivante_fbdev_driver.c
@@ -4,25 +4,32 @@
 *
 *****************************************************************************/
 
-/****************************************************************************
-*
-*    Copyright (C) 2005 - 2013 by Vivante Corp.
-*
-*    This program is free software; you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation; either version 2 of the license, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program; if not write to the Free Software
-*    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*
-*****************************************************************************/
+ /****************************************************************************
+ *
+ *    Copyright 2012 - 2015 Vivante Corporation, Santa Clara, California.
+ *    All Rights Reserved.
+ *
+ *    Permission is hereby granted, free of charge, to any person obtaining
+ *    a copy of this software and associated documentation files (the
+ *    'Software'), to deal in the Software without restriction, including
+ *    without limitation the rights to use, copy, modify, merge, publish,
+ *    distribute, sub license, and/or sell copies of the Software, and to
+ *    permit persons to whom the Software is furnished to do so, subject
+ *    to the following conditions:
+ *
+ *    The above copyright notice and this permission notice (including the
+ *    next paragraph) shall be included in all copies or substantial
+ *    portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+ *    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+ *    IN NO EVENT SHALL VIVANTE AND/OR ITS SUPPLIERS BE LIABLE FOR ANY
+ *    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ *    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *****************************************************************************/
 
 
 #include "vivante_common.h"
