a2df39b97769622fe40c8c17ad595b98f864e969
Prabhu Sundararaj <prabhu.sundararaj@nxp.com>
2017-03-20 13:44:32 -0500

    MGS-2573 [#ccc] Fix licence for create_deb.sh and fastbuild.sh
    Fix licence for create_deb.sh and fastbuild.sh to comply with
    rest of the license.
    
    As these are basic helper script files and license does not
    need at all.
    
    Date: March 20, 2017
    Signed-off-by: Prabhu Sundararaj <prabhu.sundararaj@nxp.com>

--- create_deb.sh
+++ create_deb.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-#
-#    Copyright (C) 2013 by Freescale Semiconductor, Inc.
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the license, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not write to the Free Software
-#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-#
-
 
 #export XACCVER=3.0.35-4.1.0
 #export DEBFILE=xorg1.10.4-acc-viv4.6.9p12-linux$XACCVER.deb
